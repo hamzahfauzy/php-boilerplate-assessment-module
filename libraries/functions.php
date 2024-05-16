7c7bc7cabd71fda8dec6b93efb020873e1b4140e
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-05-17 05:26:02 +0700

    add statistic

---
+++ libraries/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+use Core\Database;
+
+\Modules\Default\Libraries\Sdk\Dashboard::add(dashboardStatistic());
+
+function dashboardStatistic()
+{
+    $db = new Database;
+
+    $data = [];
+    $data['periode'] = $db->exists('assessment_periods');
+    $data['kategori'] = $db->exists('assessment_categories');
+    $data['instrumen'] = $db->exists('assessment_instruments');
+    $data['penilaian'] = $db->exists('assessment_records');
+    $data['laporan'] = $db->exists('assessment_evaluations');
+
+
+    return view('assessment/views/dashboard/statistic', compact('data'));
+}
