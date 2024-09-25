7a48912ddbda32350da97e79202e168315b48670
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-09-25 15:21:12 +0700

    update detail

---
+++ process/reports/detail.php
@@ -0,0 +1,74 @@
+<?php
+
+use Core\Database;
+use Core\Page;
+
+$db = new Database();
+$user_id = auth()->id;
+
+$period_id = $_GET['period_id'];
+$instrument_id = $_GET['instrument_id'];
+
+$user = $db->single('users', ['id' => $user_id]);
+$organization_user = $db->single('organization_users', ['user_id' => $user_id]);
+$user->organization = $db->single('organizations', ['id' => $organization_user->organization_id]);
+
+$records = $db->all('assessment_records', [
+    'period_id' => $period_id,
+    'instrument_id' => $instrument_id,
+    'user_id' => $user_id
+]);
+
+$finalReport = [];
+
+foreach($records as $data)
+{
+    $data->assessor = $db->single('users', ['id' => $data->assessor_id]);
+    $organization_user = $db->single('organization_users', ['user_id' => $data->assessor_id]);
+    $data->assessor->organization = $db->single('organizations', ['id' => $organization_user->organization_id]);
+    $data->instrument = $db->single('assessment_instruments', ['id' => $data->instrument_id]);
+    if($data->questions)
+    {
+        $data->questions = json_decode($data->questions);
+        $data->questions = end($data->questions);
+    }
+}
+
+// question categories
+$db->query = "SELECT * FROM assessment_categories WHERE id IN (SELECT category_id FROM assessment_questions WHERE instrument_id = $instrument_id GROUP BY category_id)";
+$categories = $db->exec('all');
+foreach($categories as $category)
+{
+    $questions = $db->all('assessment_questions', ['instrument_id' => $instrument_id, 'category_id' => $category->id]);
+    $finalReport[$category->id] = 0;
+    foreach($questions as $question)
+    {
+        foreach($records as $data)
+        {
+            $finalReport[$category->id] += $data->questions?->questions?->{$question->id};
+        }
+    }
+}
+
+Page::setTitle("Assesment Report Detail");
+Page::setModuleName("Report");
+Page::setBreadcrumbs([
+    [
+        'url' => routeTo('/'),
+        'title' => __('crud.label.home')
+    ],
+    [
+        'title' => 'Assessment Report',
+        'url' => routeTo('assessment/reports')
+    ],
+    [
+        'title' => 'Assessment Report Detail',
+    ]
+]);
+
+return view('assessment/views/reports/detail', [
+    'user' => $user,
+    'records' => $records,
+    'categories' => $categories,
+    'finalReport' => $finalReport,
+]);
---
+++ views/reports/detail.php
@@ -0,0 +1,145 @@
+<?php get_header() ?>
+<style>
+table td img {
+    max-width:150px;
+}
+</style>
+<div class="card">
+    <div class="card-header d-flex flex-grow-1 align-items-center">
+        <p class="h4 m-0"><?php get_title() ?></p>
+    </div>
+    <div class="card-body">
+        <table class="table mb-4">
+            <tr>
+                <td rowspan="10">1</td>
+                <td colspan="4"><b>YANG DINILAI</b></td>
+            </tr>
+            <tr>
+                <td>a. Nama</td>
+                <td><?=$user->name?></td>
+            </tr>
+            <tr>
+                <td>b. NIDN</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>c. Pangkat, Golongan ruang</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>d. Jabatan</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>- Struktural</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>- Akademik</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>e. Unit Kerja</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>- Perguruan Tinggi Swasta</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>- Fakultas</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>- Program Studi</td>
+                <td></td>
+            </tr>
+
+            <?php foreach($records as $index => $record): ?>
+            <tr>
+                <td rowspan="10"><?=$index+2?></td>
+                <td colspan="4"><b><?=ucwords($record->record_type)?> PENILAI</b></td>
+            </tr>
+            <tr>
+                <td>a. Nama</td>
+                <td><?= $record->assessor->name ?></td>
+            </tr>
+            <tr>
+                <td>b. NIDN</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>c. Pangkat, Golongan ruang</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>d. Jabatan</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>- Struktural</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>- Akademik</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>e. Unit Kerja</td>
+                <td><?= $record->assessor->organization->name?></td>
+            </tr>
+            <tr>
+                <td>- Perguruan Tinggi Swasta</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>- Fakultas</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>- Program Studi</td>
+                <td></td>
+            </tr>
+            <?php endforeach ?>
+
+            <tr>
+                <td><?= count($records)+2 ?></td>
+                <td colspan="4"><b>PENILAIAN</b></td>
+            </tr>
+            <tr>
+                <td rowspan="2"><b>UNSUR YANG DINILAI</b></td>
+                <td colspan="2"><b>NILAI</b></td>
+                <td rowspan="2"><b>KETERANGAN</b></td>
+            </tr>
+            <tr>
+                <td><b>ANGKA</b></td>
+                <td><b>SEBUTAN</b></td>
+            </tr>
+            <?php 
+            $jumlah = 0;
+            foreach($categories as $category): 
+                $jumlah+=$finalReport[$category->id];
+            ?>
+            <tr>
+                <td><?=$category->name?></td>
+                <td><?=$finalReport[$category->id]?></td>
+                <td><?=terbilang($finalReport[$category->id])?></td>
+                <td></td>
+            </tr>
+            <?php endforeach ?>
+            <tr>
+                <td>JUMLAH</td>
+                <td><?=$jumlah?></td>
+                <td><?=terbilang($jumlah)?></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>RATA-RATA</td>
+                <td><?=$jumlah/count($categories)?></td>
+                <td><?=terbilang($jumlah/count($categories))?></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</div>
+<?php get_footer() ?>
